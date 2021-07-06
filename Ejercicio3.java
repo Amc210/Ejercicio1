dc25ce07860777ad33029d1675e5c991e935268f
Amc210 <83779679+Amc210@users.noreply.github.com>
2021-07-06 12:28:07 +0200

    Add files via upload

---
+++ Ejercicio3.java
@@ -0,0 +1,11 @@
+
+public class Ejercicio3 {
+
+	public static void main(String[] args) {
+		for (int i=0; i<6; i++) 
+			System.out.print(i+" ");
+			System.out.println("FIN DE PROGRAMA");
+
+	}
+
+}
